59b28358d1fb9530db1a761378d49b9ea12fb64f
Thôi Đặng Trường Thọ <62749662+truongthodeptrai@users.noreply.github.com>
2025-07-08 19:43:32 +0700

    DFS_solver.py

--- Source/DFS_solver.py
+++ Source/DFS_solver.py
@@ -1,41 +1,47 @@
+# DFS_solver.py
 from modelCar import Car
 
 BOARD_SIZE = 6
+
 
 def print_board(board):
     for row in board:
         print(' '.join(row))
     print()
 
+
 def get_cars(board):
     cars = []
-    visited = []
+    visited = set()
     for r in range(BOARD_SIZE):
         for c in range(BOARD_SIZE):
             cell = board[r][c]
-            if cell == '.' or [r, c] in visited:
+            if cell == '.' or (r, c) in visited:
                 continue
+            # determine orientation and length
+            length = 1
             if c + 1 < BOARD_SIZE and board[r][c + 1] == cell:
+                # horizontal
                 length = 2
                 if c + 2 < BOARD_SIZE and board[r][c + 2] == cell:
-                        length = 3
+                    length = 3
                 for i in range(length):
-                    visited.append([r, c + 1])
+                    visited.add((r, c + i))
                 cars.append(Car(cell, r, c, length, True, None))
             elif r + 1 < BOARD_SIZE and board[r + 1][c] == cell:
+                # vertical
                 length = 2
                 if r + 2 < BOARD_SIZE and board[r + 2][c] == cell:
                     length = 3
                 for i in range(length):
-                    visited.append([r + i, c])
+                    visited.add((r + i, c))
                 cars.append(Car(cell, r, c, length, False, None))
     return cars
 
-def clone_board(board):
-    return [row[:] for row in board]
 
 def clone_cars(cars):
     return [Car(c.name, c.row, c.col, c.length, c.horizontal, c.color) for c in cars]
+
 
 def place_cars(cars):
     board = [['.' for _ in range(BOARD_SIZE)] for _ in range(BOARD_SIZE)]
@@ -47,102 +53,86 @@ def place_cars(cars):
                 board[car.row + i][car.col] = car.name
     return board
 
+
 def get_possible_moves(cars):
     moves = []
-    for idx in range(len(cars)):
-        car = cars[idx]
+    base = place_cars(cars)
+    for idx, car in enumerate(cars):
+        # horizontal moves
         if car.horizontal:
-            if car.col > 0:
-                board = place_cars(cars)
-                if board[car.row][car.col - 1] == '.':
-                    new_cars = clone_cars(cars)
-                    new_cars[idx].col -= 1
-                    moves.append((car.name, ' Left', new_cars))
-            if car.col + car.length < BOARD_SIZE:
-                board = place_cars(cars)
-                if board[car.row][car.col + car.length] == '.':
-                    new_cars = clone_cars(cars)
-                    new_cars[idx].col += 1
-                    moves.append((car.name, 'Right', new_cars))
+            # move left
+            if car.col > 0 and base[car.row][car.col - 1] == '.':
+                new = clone_cars(cars)
+                new[idx].col -= 1
+                moves.append((car.name, 'Left', new))
+            # move right
+            if car.col + car.length < BOARD_SIZE and base[car.row][car.col + car.length] == '.':
+                new = clone_cars(cars)
+                new[idx].col += 1
+                moves.append((car.name, 'Right', new))
         else:
-            if car.row > 0:
-                board = place_cars(cars)
-                if board[car.row - 1][car.col] == '.':
-                    new_cars = clone_cars(cars)
-                    new_cars[idx].row -= 1
-                    moves.append((car.name, ' Up', new_cars))
-            if car.row + car.length < BOARD_SIZE:
-                board = place_cars(cars)
-                if board[car.row + car.length][car.col] == '.':
-                    new_cars = clone_cars(cars)
-                    new_cars[idx].row += 1
-                    moves.append((car.name, ' Down', new_cars))
+            # vertical moves
+            if car.row > 0 and base[car.row - 1][car.col] == '.':
+                new = clone_cars(cars)
+                new[idx].row -= 1
+                moves.append((car.name, 'Up', new))
+            if car.row + car.length < BOARD_SIZE and base[car.row + car.length][car.col] == '.':
+                new = clone_cars(cars)
+                new[idx].row += 1
+                moves.append((car.name, 'Down', new))
     return moves
 
+
 def is_goal(cars):
+    # check if the red car (name 'r' or 'R') has exited the board on the right
     for car in cars:
-        if car.name == 'X' and car.col + car.length == BOARD_SIZE:
+        if car.name.lower() == 'r' and car.col + car.length == BOARD_SIZE:
             return True
     return False
 
-def serialize(cars):
-    return [(c.name, c.row, c.col) for c in cars]
 
-def dfs_solver(cars):
+def serialize(cars):
+    # unique representation for visited checking
+    return tuple((c.name, c.row, c.col) for c in sorted(cars, key=lambda x: x.name))
+
+
+def dfs_solver(cars, max_depth=10000):
     stack = [(cars, [])]
-    visited = []
+    visited = set()
+
     while stack:
         state, path = stack.pop()
         key = serialize(state)
-        if key in visited:
+        if key in visited or len(path) > max_depth:
             continue
-        visited.append(key)
+        visited.add(key)
 
         if is_goal(state):
-            return path + [(None, None, state)]
+            return path
 
-        for name, dirc, next_cars in get_possible_moves(state):
-            move = (name, dirc, next_cars)
-            stack.append((next_cars, path + [move]))
+        for name, direction, next_cars in get_possible_moves(state):
+            stack.append((next_cars, path + [(name, direction)]))
     return None
 
-def show_solution(solution):
-    for i in range(len(solution)):
-        name, dirc, cars = solution[i]
-        print("Step", i, end=": ")
-        if name is None:
-            print("Final state")
-        else:
-            print("Move", name, dirc)
-        print_board(place_cars(cars))
 
-'''
-def main():
-    initial_board = [
-        ['.', '.', '.', '.', 'B', 'F'],
-        ['A', 'A', 'I', 'E', 'B', 'F'],
-        ['X', 'X', 'I', 'E', 'B', 'H'],
-        ['.', '.', 'C', 'E', '.', 'H'],
-        ['.', '.', 'C', 'D', 'D', 'H'],
-        ['.', '.', '.', '.', '.', '.']
-    ]
-    print("Initial board: ")
-    print_board(initial_board)
-
-    initial_cars = get_cars(initial_board)
-
-    solution = None
-    method = 'dfs'
-    if method == 'dfs':
-        solution = dfs_solver(initial_cars)
-
-    if solution:
-        print("Solution found using", method.upper())
-        show_solution(solution)
-    else:
-        print("No solution found.")
-
-if __name__ == '__main__':
-    main()
-'''
+def show_solution(moves, initial_board):
+    cars = get_cars(initial_board)
+    board = place_cars(cars)
+    print_board(board)
+    for i, (name, direction) in enumerate(moves, 1):
+        print(f"Step {i}: Move {name} {direction}")
+        # update cars
+        for car in cars:
+            if car.name == name:
+                if direction == 'Left':
+                    car.col -= 1
+                elif direction == 'Right':
+                    car.col += 1
+                elif direction == 'Up':
+                    car.row -= 1
+                elif direction == 'Down':
+                    car.row += 1
+                break
+        board = place_cars(cars)
+        print_board(board)
 
