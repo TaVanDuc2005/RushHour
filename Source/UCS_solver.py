1ee089634322b76977c7e4e20ad6d9676a3d1645
TaVanDuc2005 <tavanduc298@gmail.com>
2025-07-13 21:04:57 +0700

    Update ucs

--- Source/UCS_solver.py
+++ Source/UCS_solver.py
@@ -11,68 +11,68 @@ def is_goal_state(cars):
             return car.row == 2 and car.col + car.length == 6
     return False
 
+def is_blocked(moving_car, target_row, target_col, cars):
+    for other in cars:
+        if other == moving_car:
+            continue
+        if moving_car.horizontal:
+            if other.horizontal:
+                if other.row != moving_car.row:
+                    continue
+                if not (target_col + moving_car.length <= other.col or target_col >= other.col + other.length):
+                    return True
+            else:
+                for i in range(moving_car.length):
+                    if (other.col == target_col + i and
+                        other.row <= moving_car.row < other.row + other.length):
+                        return True
+        else:
+            if not other.horizontal:
+                if other.col != moving_car.col:
+                    continue
+                if not (target_row + moving_car.length <= other.row or target_row >= other.row + other.length):
+                    return True
+            else:
+                for i in range(moving_car.length):
+                    if (other.row == target_row + i and
+                        other.col <= moving_car.col < other.col + other.length):
+                        return True
+    return False
+
+
 def get_successors(state):
     successors = []
 
     for i, car in enumerate(state):
         if car.horizontal:
             new_col = car.col - 1
-            if new_col >= 0:
-                blocked = any(
-                    other.row == car.row and not (
-                        other.col + other.length <= new_col or other.col >= new_col + car.length
-                    )
-                    for j, other in enumerate(state) if j != i
-                )
-                if not blocked:
-                    new_state = copy.deepcopy(state)
-                    new_state[i].col = new_col
-                    successors.append((new_state, car.length))
+            if new_col >= 0 and not is_blocked(car, car.row, new_col, state):
+                new_state = copy.deepcopy(state)
+                new_state[i].col = new_col
+                successors.append((new_state, car.length))
 
             new_col = car.col + 1
-            if new_col + car.length <= 6:
-                blocked = any(
-                    other.row == car.row and not (
-                        other.col + other.length <= new_col or other.col >= new_col + car.length
-                    )
-                    for j, other in enumerate(state) if j != i
-                )
-                if not blocked:
-                    new_state = copy.deepcopy(state)
-                    new_state[i].col = new_col
-                    successors.append((new_state, car.length))
-                    successors.append((new_state, car.length))
-
+            if new_col + car.length <= 6 and not is_blocked(car, car.row, new_col, state):
+                new_state = copy.deepcopy(state)
+                new_state[i].col = new_col
+                successors.append((new_state, car.length))
         else:
             new_row = car.row - 1
-            if new_row >= 0:
-                blocked = any(
-                    other.col == car.col and not (
-                        other.row + other.length <= new_row or other.row >= new_row + car.length
-                    )
-                    for j, other in enumerate(state) if j != i
-                )
-                if not blocked:
-                    new_state = copy.deepcopy(state)
-                    new_state[i].row = new_row
-                    successors.append((new_state, car.length))
+            if new_row >= 0 and not is_blocked(car, new_row, car.col, state):
+                new_state = copy.deepcopy(state)
+                new_state[i].row = new_row
+                successors.append((new_state, car.length))
 
             new_row = car.row + 1
-            if new_row + car.length <= 6:
-                blocked = any(
-                    other.col == car.col and not (
-                        other.row + other.length <= new_row or other.row >= new_row + car.length
-                    )
-                    for j, other in enumerate(state) if j != i
-                )
-                if not blocked:
-                    new_state = copy.deepcopy(state)
-                    new_state[i].row = new_row
-                    successors.append((new_state, car.length))
+            if new_row + car.length <= 6 and not is_blocked(car, new_row, car.col, state):
+                new_state = copy.deepcopy(state)
+                new_state[i].row = new_row
+                successors.append((new_state, car.length))
 
     return successors
 
 
+# Uniform-Cost Search solver
 def ucs_solver(initial_state):
     frontier = []
     visited = set()
@@ -89,6 +89,9 @@ def ucs_solver(initial_state):
         visited.add(state_id)
 
         if is_goal_state(current_state):
+            print("Goal reached!")
+            print("Steps:", len(path) - 1)
+            print("Total cost:", cost)
             return path
 
         for next_state, step_cost in get_successors(current_state):
@@ -101,4 +104,5 @@ def ucs_solver(initial_state):
                     path + [copy.deepcopy(next_state)]
                 ))
 
+    print("No solution found.")
     return None
